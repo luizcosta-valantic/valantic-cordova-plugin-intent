75850518be58f17f01f588a52b8755f936151665
luizcosta-valantic <luiz.costa@lcs.valantic.com>
2025-07-25 13:28:24 -0300

    Update after_prepare_add_receiver.js

--- hooks/after_prepare_add_receiver.js
+++ hooks/after_prepare_add_receiver.js
@@ -7,7 +7,7 @@ module.exports = function (context) {
 
     // 1️⃣ Receiver block
     const receiverDeclaration = `
-        <receiver android:exported="true" android:name="com.darryncampbell.cordova.plugin.intent.MyBroadcastReceiver">
+        <receiver android:exported="true" android:enabled="true" android:name="com.darryncampbell.cordova.plugin.intent.MyBroadcastReceiver">
             <intent-filter>
                 <action android:name="com.symbol.datawedge.api.RESULT_ACTION" />
                 <action android:name="com.valantic.outsystems.SCAN_RESULT" />
